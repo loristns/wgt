acf8883f38f2193ebdaff929469306df80a24e8c
loristns <11007033+loristns@users.noreply.github.com>
2023-09-24 18:48:53 +0200

    feat: add gelu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {WGT} from './lib';
 import {Tensor} from './lib/tensor';
-import {Input, Matmul, Softmax} from './lib/ops';
+import {Gelu, Input, Matmul, Softmax} from './lib/ops';
 
 async function run() {
   await WGT.initializeGpu();
@@ -13,6 +13,7 @@ async function run() {
   const matmul2 = new Matmul(matmul, matmul);
 
   const softmax = new Softmax(matmul);
+  const gelu = new Gelu(matmul);
 
   // Input data
   const a = Tensor.fromArray(
@@ -21,7 +22,7 @@ async function run() {
       .map(() =>
         Array(1024)
           .fill(1)
-          .map(() => Math.random())
+          .map(() => Math.random() * 0.001)
       )
   );
 
@@ -31,21 +32,21 @@ async function run() {
       .map(() =>
         Array(1024)
           .fill(1)
-          .map(() => Math.random())
+          .map(() => Math.random() * 0.1)
       )
   );
 
   // Run
-  console.time('matmul_2d');
-
   input1.write(a);
   input2.write(b);
 
-  const [output1, output2] = await WGT.run([matmul, softmax]);
-
+  console.time('matmul_2d');
+  const [output1, output2, output3] = await WGT.run([matmul, softmax, gelu]);
   console.timeEnd('matmul_2d');
+
   console.log(output1.data);
   console.log(output2.data);
+  console.log(output3.data);
 
   // Cleanup
   input1.clean();
@@ -53,6 +54,7 @@ async function run() {
   matmul.clean();
   matmul2.clean();
   softmax.clean();
+  gelu.clean();
 }
 
 function App() {
--- src/lib/ops.ts
+++ src/lib/ops.ts
@@ -152,6 +152,16 @@ export class Matmul extends Op {
   }
 }
 
+/**
+ * A softmax operation for each row of a tensor.
+ *
+ * The softmax function is defined as:
+ * ```
+ * softmax(x) = exp(x - max(x)) / sum(exp(x - max(x)))
+ * ```
+ * This implementation is probably suboptimal, as it does not parallelize the sum and max operations.
+ * TODO: Implement a faster kernel.
+ */
 export class Softmax extends Op {
   pipeline: GPUComputePipeline;
 
@@ -218,6 +228,61 @@ export class Softmax extends Op {
         pipeline: this.pipeline,
         params: [this.dependencies[0].buffer, this.buffer],
         workgroups: [this.shape.rows, 1, 1],
+      },
+    ];
+  }
+}
+
+/**
+ * A GEneralized Linear Unit (GELU) operation over each element of a tensor.
+ */
+export class Gelu extends Op {
+  pipeline: GPUComputePipeline;
+
+  constructor(input: Op) {
+    const shape = input.shape;
+    super(shape, [input]);
+
+    this.pipeline = WGT.device.createComputePipeline({
+      layout: 'auto',
+      compute: {
+        module: WGT.device.createShaderModule({
+          code: /* wgsl */ `
+            ${WGSL_TENSOR_TYPE}
+      
+            // Input
+            @group(0) @binding(0) var<storage, read> input: Tensor;
+            
+            // Output
+            @group(0) @binding(1) var<storage, read_write> result: Tensor;
+           
+            @compute @workgroup_size(64) fn main(
+              @builtin(global_invocation_id) id: vec3<u32>,
+            ) {
+              result.rows = input.rows;
+              result.cols = input.cols;
+              
+              let row = id.x;
+              let col = id.y;
+              let value = input.matrix[row * input.cols + col];
+            
+              result.matrix[row * result.cols + col] = \
+                0.5 * value * (1 + tanh(0.797884 * (value + 0.044715 * pow(value, 3.0))));
+            }
+          `,
+        }),
+        entryPoint: 'main',
+      },
+    });
+  }
+
+  getCommands(): OpCommand[] {
+    return [
+      ...super.getCommands(),
+      {
+        pipeline: this.pipeline,
+        params: [this.dependencies[0].buffer, this.buffer],
+        workgroups: [this.shape.rows, this.shape.cols],
       },
     ];
   }

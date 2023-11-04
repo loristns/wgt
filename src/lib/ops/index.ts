453e6ab636d926f92466abf7cc050dea06c7a34b
loristns <11007033+loristns@users.noreply.github.com>
2023-11-04 16:20:12 +0100

    chore: fresh start

--- src/lib/ops/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './op';
-
-export * from './gelu';
-export * from './gemm';
-export * from './input';
-export * from './layerNorm';
-export * from './selfAttention';
-export * from './softmax';
-export * from './transpose';

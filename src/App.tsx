b7217187d1cf534f77eb90ac4b6617237af77992
loristns <11007033+loristns@users.noreply.github.com>
2023-11-18 13:47:41 +0100

    feat: add transpose

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
 import {WGT} from './wgt/wgt';
 import {Tensor} from './wgt/tensor';
 import {input} from './wgt/ops/input';
+import {layerNorm, LayerNormParameters} from './wgt/ops/layerNorm';
 import {linear, LinearParameters} from './wgt/ops/linear';
 import {gelu} from './wgt/ops/gelu';
+import {softmax} from './wgt/ops/softmax';
+import {transpose} from './wgt/ops/transpose';
 
 async function run() {
   await WGT.initializeGpu();
 
-  const params: LinearParameters = {
+  const linear1Params: LinearParameters = {
     weights: Tensor.random({batches: 1, rows: 10, cols: 200}),
     bias: Tensor.zeros({batches: 1, rows: 1, cols: 200}),
   };
 
+  const layerNorm1Params: LayerNormParameters = {
+    scale: Tensor.ones({batches: 1, rows: 1, cols: 200}),
+    bias: Tensor.zeros({batches: 1, rows: 1, cols: 200}),
+  };
+
   const input1 = input({batches: 1, rows: 1, cols: 10});
-  const linear1 = linear(input1, params);
+  const linear1 = linear(input1, linear1Params);
+  const layerNorm1 = transpose(layerNorm(linear1, layerNorm1Params));
 
-  const input2 = input({batches: 1, rows: 1, cols: 1});
+  const input2 = input({batches: 1, rows: 1, cols: 5});
   const gelu1 = gelu(input2);
+  const softmax1 = softmax(gelu1);
 
-  const graph = new WGT([input1, input2], [linear1, gelu1]);
+  const graph = new WGT(
+    [input1, input2],
+    [linear1, layerNorm1, gelu1, softmax1]
+  );
 
   const a = Tensor.random({batches: 1, rows: 1, cols: 10});
-  const b = Tensor.ones({batches: 1, rows: 1, cols: 1});
+  const b = Tensor.random({batches: 1, rows: 1, cols: 5});
   console.log(await graph.run([a, b]));
 
   graph.destroy();
---
+++ src/wgt/ops/transpose.ts
@@ -0,0 +1,49 @@
+import {Shape, Tensor} from '../tensor';
+import {DeviceTensor} from '../deviceTensor';
+import {Op} from '../op';
+
+/**
+ * A transpose operation (swaps rows and columns).
+ */
+export function transpose(input: DeviceTensor): DeviceTensor {
+  const shape = new Shape({
+    rows: input.shape.cols,
+    cols: input.shape.rows,
+    batches: input.shape.batches,
+  });
+
+  const output = new DeviceTensor(shape);
+  output.sourceOp = new Op({
+    inputs: [input],
+    outputs: [output],
+    workgroups: [
+      Math.ceil(shape.rows / 16),
+      Math.ceil(shape.cols / 16),
+      shape.batches,
+    ],
+    code: /* wgsl */ `
+      ${Tensor.WGSL}
+        
+      // Input
+      @group(0) @binding(0) var<storage, read> input: Tensor;
+
+      // Output
+      @group(0) @binding(1) var<storage, read_write> result: Tensor;
+
+      @compute @workgroup_size(16, 16, 1) fn main(
+        @builtin(global_invocation_id) id: vec3<u32>,
+      ) {
+        result.shape = Shape(input.shape.batches, input.shape.cols, input.shape.rows);
+
+        let batch = id.z;
+        let row = id.x;
+        let col = id.y;
+
+        result.tensor[tensor_idx(result.shape, batch, row, col)] = \
+          input.tensor[tensor_idx(input.shape, batch, col, row)];
+      }
+    `,
+  });
+
+  return output;
+}
